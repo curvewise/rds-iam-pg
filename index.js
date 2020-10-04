9a540a7293417067572d0eb1841f31782f95950b
Jacob Beard <jake@minnow.io>
2020-10-03 22:50:58 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { postgraphile } = require("postgraphile");
+
+const app = express();
+
+app.use(
+  postgraphile(
+    process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/postgres",
+    "public",
+    {
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+    }
+  )
+);
+
+app.listen(process.env.PORT || 5000);
---
+++ start.sh
@@ -0,0 +1,1 @@
+npx postgraphile -c 'postgresql://postgres:postgres@localhost:5432/postgres' --watch --enhance-graphiql --dynamic-json  --allow-explain
