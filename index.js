e234cc614a75571161051cdf38744ce4b7dfee01
Jacob Beard <jake@minnow.io>
2020-10-31 19:42:03 -0400

    S3 list import bucket (#14)
    support for listing the contents of the goldilocks upload bucket via graphql

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const { postgraphile } = require('postgraphile')
 const cors = require('cors')
 const Joi = require('joi')
 const { configSchema } = require('./src/config-schema')
+const AWS = require('aws-sdk')
+const { makeExtendSchemaPlugin, gql } = require('graphile-utils')
+const util = require('util')
 
-const { databaseUrl } = Joi.attempt(
+const { databaseUrl, awsProfile, importBucket } = Joi.attempt(
   require('config').util.toObject(),
   configSchema
 )
+const myConfig = new AWS.Config({
+  credentials: awsProfile
+    ? new AWS.SharedIniFileCredentials({ profile: awsProfile })
+    : undefined,
+})
+const s3 = new AWS.S3(myConfig)
+const listObjects = util.promisify(s3.listObjects.bind(s3))
 
 const app = express()
 
@@ -17,11 +27,45 @@ app.get('/', (req, res) => res.send('Goldilocks graphql server'))
 
 app.use(cors())
 
+const GoldilocksUploadBucketListPlugin = makeExtendSchemaPlugin(build => {
+  return {
+    typeDefs: gql`
+      type GoldilocksUploadBucketListResponse {
+        IsTruncated: Boolean
+        Marker: String
+        Name: String
+        Prefix: String
+        MaxKeys: Int
+        Contents: [GoldilocksUploadBucketListResponseContents]
+      }
+
+      type GoldilocksUploadBucketListResponseContents {
+        Key: String
+        LastModified: Date
+        ETag: String
+        Size: Int
+        StorageClass: String
+      }
+
+      extend type Query {
+        uploadBucketList: GoldilocksUploadBucketListResponse
+      }
+    `,
+    resolvers: {
+      Query: {
+        uploadBucketList: async () =>
+          await listObjects({ Bucket: importBucket }),
+      },
+    },
+  }
+})
+
 app.use(
   postgraphile(databaseUrl, 'public', {
     watchPg: true,
     graphiql: true,
     enhanceGraphiql: true,
+    appendPlugins: [GoldilocksUploadBucketListPlugin],
   })
 )
 
--- src/config-schema.js
+++ src/config-schema.js
@@ -4,6 +4,8 @@ const Joi = require('joi')
 
 const configSchema = Joi.object({
   databaseUrl: Joi.string().uri({ scheme: ['postgres', 'postgresql'] }),
+  awsProfile: Joi.string(),
+  importBucket: Joi.string().required(),
 }).required()
 
 module.exports = { configSchema }
