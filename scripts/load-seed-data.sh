ba755abd920e6cf309d15a84eeaf0176e7beeace
Paul Melnikow <github@paulmelnikow.com>
2020-11-06 12:56:39 -0500

    Fix DDL syntax, stop on errors when loading seed data, and match Heroku PG version (#22)

--- scripts/load-seed-data.sh
+++ scripts/load-seed-data.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 psql $DATABASE_URL \
+    -v ON_ERROR_STOP=on \
     -f src/reset.sql \
     -f src/ddl.sql \
     -f src/seed_data.sql 
--- src/ddl.sql
+++ src/ddl.sql
@@ -26,22 +26,10 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON DATABASE postgres IS 'default administrative connection database';
-
---
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 SET default_tablespace = '';
 
@@ -52,7 +40,7 @@ SET default_with_oids = false;
 --
 
 CREATE TYPE public.body_part_type AS ENUM (
-    'body',
+    'body'
 );
 
 CREATE TYPE public.topology_type AS ENUM (
